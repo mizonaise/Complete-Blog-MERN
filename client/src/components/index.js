9b6d136fa1f2b4ef40ad2c499f20dd545b0a20cb
mizonaise <mizonaise@gmail.com>
2021-03-04 16:16:54 +0100

    create Components

---
+++ client/src/components/index.js
@@ -0,0 +1,5 @@
+export { default as Header } from "./Header";
+export { default as Slider } from "./Slider";
+export { default as FrechStories } from "./FrechStories";
+export { default as TrendingPosts } from "./TrendingPosts";
+export { default as Footer } from "./Footer";
--- client/src/screens/Home.jsx
+++ client/src/screens/Home.jsx
@@ -1,6 +1,6 @@
 import { Header } from "./components";
 
-function App() {
+function HomeScreen() {
   return (
     <>
       <Header />
@@ -8,4 +8,4 @@ function App() {
   );
 }
 
-export default App;
+export default HomeScreen;
