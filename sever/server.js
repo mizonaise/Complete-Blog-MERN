eea35a51a8fd0f9118779713d7665257233eb725
mizonaise <mizonaise@gmail.com>
2021-03-06 00:28:37 +0100

    Setting up express and MongoDb, create models schema

---
+++ sever/server.js
@@ -0,0 +1,38 @@
+require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+// Connect Database
+const db = process.env.MONGODB_URL;
+
+mongoose.connect(db, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.on("connected", () => {
+  console.log("Connected to MongoDB");
+});
+
+mongoose.connection.on("error", (error) => {
+  console.log("Error: " + error);
+});
+
+//Models import
+require("./models/Post");
+require("./models/Category");
+require("./models/Comment");
+
+// Init Middleware
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+var server = app.listen(PORT, function () {
+  var port = server.address().port;
+  var host = server.address().address;
+  console.log("Server is started at " + PORT);
+  console.log("running at http://" + host + ":" + port);
+});
